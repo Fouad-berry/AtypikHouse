d310a526471d6ec9f9826c792d0b704ac8ca2b37
Fouad-berry <fidriss96@gmail.com>
2024-07-15 21:50:01 +0200

    update categories

--- app/components/NavBar/Categories.tsx
+++ app/components/NavBar/Categories.tsx
@@ -64,7 +64,7 @@ export const categories = [
     {
         label: 'Arctic',
         icon: BsSnow,
-        description:'La propriété est moderne'
+        description:'La propriété est vraiment moderne'
     },
     {
         label: 'Cave',
