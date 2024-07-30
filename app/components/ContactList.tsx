3620b40e97366e0f2ee450b99cec042b03440840
Fouad-berry <fidriss96@gmail.com>
2024-07-31 00:23:27 +0200

    add style to message system

--- app/components/ContactList.tsx
+++ app/components/ContactList.tsx
@@ -1,6 +1,7 @@
 // components/ContactList.tsx
-import { SafeUser, Message } from '@/app/types';
+import { SafeUser } from '@/app/types';
 import { useRouter } from 'next/navigation';
+import { FaUserCircle } from 'react-icons/fa';
 
 interface ContactListProps {
   contacts: SafeUser[];
@@ -15,12 +16,21 @@ const ContactList: React.FC<ContactListProps> = ({ contacts, currentUser }) => {
   };
 
   return (
-    <div className="max-w-sm w-full border rounded-lg p-4 mb-4 shadow-lg">
-      <h2 className="text-xl font-bold mb-4">Contacts</h2>
-      <ul>
+    <div className="max-w-sm w-full border rounded-lg p-4 mb-4 shadow-lg bg-white">
+      <h2 className="text-2xl font-bold mb-4 text-gray-800">Contacts</h2>
+      <ul className="space-y-2">
         {contacts.map((contact) => (
-          <li key={contact.id} onClick={() => handleContactClick(contact.id)} className="cursor-pointer mb-2">
-            {contact.name}
+          <li
+            key={contact.id}
+            onClick={() => handleContactClick(contact.id)}
+            className="cursor-pointer p-2 rounded-lg hover:bg-blue-50 flex items-center transition duration-200 ease-in-out border-b border-gray-200"
+          >
+            <div className="flex-shrink-0 h-12 w-12 rounded-full bg-gray-300 flex items-center justify-center text-xl font-semibold text-white">
+              {contact.name ? contact.name.charAt(0) : <FaUserCircle className="text-gray-400" />}
+            </div>
+            <div className="ml-3">
+              <p className="text-lg font-medium text-gray-700">{contact.name ?? 'Nom inconnu'}</p>
+            </div>
           </li>
         ))}
       </ul>
