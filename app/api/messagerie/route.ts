bd01ce27fe3f9d66186695d3117295049ec9ba20
Fouad-berry <fidriss96@gmail.com>
2024-07-31 22:36:10 +0200

    update message logic

--- app/api/messagerie/route.ts
+++ app/api/messagerie/route.ts
@@ -3,31 +3,26 @@ import prisma from '@/app/libs/prismadb';
 import getCurrentUser from '@/app/actions/getCurrentUser';
 
 export async function POST(request: Request) {
-  try {
-    const currentUser = await getCurrentUser();
+  const currentUser = await getCurrentUser();
 
-    if (!currentUser) {
-      return NextResponse.error();
-    }
-
-    const body = await request.json();
-    const { ownerId, message } = body;
-
-    if (!ownerId || !message) {
-      return NextResponse.error();
-    }
-
-    const newMessage = await prisma.message.create({
-      data: {
-        content: message,
-        senderId: currentUser.id,
-        receiverId: ownerId,
-      },
-    });
-
-    return NextResponse.json(newMessage);
-  } catch (error) {
-    console.error('Error sending message:', error);
+  if (!currentUser) {
     return NextResponse.error();
   }
+
+  const body = await request.json();
+  const { ownerId, message } = body;
+
+  if (!ownerId || !message) {
+    return NextResponse.error();
+  }
+
+  const newMessage = await prisma.message.create({
+    data: {
+      content: message,
+      senderId: currentUser.id,
+      receiverId: ownerId,
+    },
+  });
+
+  return NextResponse.json(newMessage);
 }
