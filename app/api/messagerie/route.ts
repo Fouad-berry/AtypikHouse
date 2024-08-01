aad6f3e5d79146f316b41a793ca16abb571d97df
Fouad-berry <fidriss96@gmail.com>
2024-08-01 08:56:37 +0200

    remove messagclient and page.txt on mes-messages to see her behaviour

--- app/api/messagerie/route.ts
+++ app/api/messagerie/route.ts
@@ -3,26 +3,31 @@ import prisma from '@/app/libs/prismadb';
 import getCurrentUser from '@/app/actions/getCurrentUser';
 
 export async function POST(request: Request) {
-  const currentUser = await getCurrentUser();
+  try {
+    const currentUser = await getCurrentUser();
 
-  if (!currentUser) {
+    if (!currentUser) {
+      return NextResponse.error();
+    }
+
+    const body = await request.json();
+    const { ownerId, message } = body;
+
+    if (!ownerId || !message) {
+      return NextResponse.error();
+    }
+
+    const newMessage = await prisma.message.create({
+      data: {
+        content: message,
+        senderId: currentUser.id,
+        receiverId: ownerId,
+      },
+    });
+
+    return NextResponse.json(newMessage);
+  } catch (error) {
+    console.error('Error sending message:', error);
     return NextResponse.error();
   }
-
-  const body = await request.json();
-  const { ownerId, message } = body;
-
-  if (!ownerId || !message) {
-    return NextResponse.error();
-  }
-
-  const newMessage = await prisma.message.create({
-    data: {
-      content: message,
-      senderId: currentUser.id,
-      receiverId: ownerId,
-    },
-  });
-
-  return NextResponse.json(newMessage);
 }
