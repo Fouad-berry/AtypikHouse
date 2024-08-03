591c9bf288da3cc5a27285ed94577db8888a0c72
Fouad-berry <fidriss96@gmail.com>
2024-08-03 11:24:47 +0200

    add style to summary for user friendly interface

--- app/summary/RecapClient.tsx
+++ app/summary/RecapClient.tsx
@@ -1,29 +1,22 @@
 'use client';
 
 import { useRouter } from "next/navigation";
-
-import Container from "../components/Container";
-import Heading from "../components/Heading";
-import { SafeListing, SafeReservation, SafeUser } from "../types";
 import { useCallback, useState } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
-import ListingCard from "../components/Listings/ListingCard";
+import Container from "../components/Container";
+import Heading from "../components/Heading";
+import { SafeReservation, SafeUser } from "../types";
 import PayPalButton from "../components/PaypalButton";
 
 interface RecapClientProps {
     reservations: SafeReservation[];
     currentUser?: SafeUser;
-/*     listing: SafeListing & {
-        user:SafeUser
-    };
- */
 }
 
 const RecapClient: React.FC<RecapClientProps> = ({
     reservations,
-/*     listing,
- */    currentUser
+    currentUser
 }) => {
     const totalPriceString = sessionStorage.getItem('totalPrice');
     const totalPrice = totalPriceString ? parseInt(totalPriceString) : 0;
@@ -61,82 +54,64 @@ const RecapClient: React.FC<RecapClientProps> = ({
     const onFinal = useCallback(async () => {
         // Récupérer l'ID de l'utilisateur actuel et l'ID du listing depuis le sessionStorage
         const userId = currentUser?.id;
-/*         const listingId = listing?.id
- */
-/*         const listingId = sessionStorage.getItem('listingId');
- */
+
         // Envoyer une requête POST à votre backend pour enregistrer le paiement
         try {
             await axios.post('/api/payment', {
                 userId,
                 totalPrice
             });
-            toast.success('Votre payement a été accepté et votre reservation confirmé');
+            toast.success('Votre paiement a été accepté et votre réservation confirmée');
             router.push(`/trips`);
         } catch (error) {
             console.error("Erreur lors de l'enregistrement du paiement :", error);
-            toast.error("Quelque chose s`est mal passé");
+            toast.error("Quelque chose s'est mal passé");
         }
     }, [currentUser, totalPrice, router]);
         
     return (
         <Container>
-            <br></br>
-        <div className="col-span-4 flex flex-col gap-8">
-            <div className="flex flex-col gap-2">
-                <div className="
-                    text-xl
-                    font-semibold
-                    flex
-                    flex-col
-                    gap-2
-                ">
-                    <div style={{fontSize:30}}>Confirmer et Payer </div><br></br>
-                    <div style={{fontSize:18, fontWeight:"bolder"}}>Recapitulatif de votre voyage </div>
-                </div><br></br>
-                <div className="
-                    flex
-                    flex-col
-                    gap-4
-                    font-normal
-                ">
-                    <div>
-                        <p style={{fontWeight:"bold"}}>Durée du voyage:</p>
-                            {formattedStartDate} - {formattedEndDate}
+            <div className="bg-white p-8 rounded-lg shadow-lg">
+                <Heading title="Voilà le recapitulatif de votre commande" subtitle="" />
+                <div className="flex flex-col gap-6">
+                    <div className="text-center">
+                        <h2 className="text-2xl font-bold mb-4">Confirmer et Payer</h2>
+                        <p className="text-lg font-semibold">Récapitulatif de votre voyage</p>
                     </div>
-                    <div>
-                        <p style={{fontWeight:"bold"}}>Voyageurs: </p>
+                    <div className="flex flex-col gap-4 p-4 bg-gray-100 rounded-lg">
+                        <div>
+                            <p className="font-bold text-lg">Durée du voyage:</p>
+                            <p>{formattedStartDate} - {formattedEndDate}</p>
+                        </div>
+                        <div>
+                            <p className="font-bold text-lg">Voyageurs:</p>
+                            <p>{reservations.length}</p>
+                        </div>
+                        <div>
+                            <p className="font-bold text-lg">Prix total:</p>
+                            <p>{totalPrice} €</p>
+                        </div>
                     </div>
-                    <div>
-                        <p style={{fontWeight:"bold"}}>Prix total:</p>
-                            {totalPrice} €
+                    <hr />
+                    <div className="flex flex-col gap-4 p-4 bg-gray-100 rounded-lg">
+                        <h2 className="text-xl font-semibold">Conditions d'annulation</h2>
+                        <p>Annulation gratuite pendant 48 heures. Si vous annulez avant le 25 mai, vous aurez droit à un remboursement partiel. En savoir plus</p>
+                    </div>
+                    <hr />
+                    <div className="flex flex-col gap-4 p-4 bg-gray-100 rounded-lg">
+                        <h2 className="text-xl font-semibold">Règles de base</h2>
+                        <p>Nous demandons à tous les voyageurs de se souvenir de quelques règles simples qui contribueront à rendre leur séjour agréable, pour eux comme pour les hôtes.</p>
+                        <ul className="list-disc list-inside">
+                            <li>Respectez le règlement intérieur.</li>
+                            <li>Traitez le logement de votre hôte comme si c'était le vôtre.</li>
+                        </ul>
+                    </div>
+                    <hr />
+                    <div className="flex justify-center">
+                        <PayPalButton totalPrice={totalPrice} onSuccess={onFinal} />
                     </div>
                 </div>
             </div>
-            <hr />
-                <div>
-                    <h2 className="text-xl font-semibold text-normal-700">Conditions d&apos;annulation</h2> <br></br>
-                    <p>
-                    Annulation gratuite pendant 48 heures. Si vous annulez avant le 25 mai, <p></p> 
-                    vous aurez droit à un remboursement partiel. En savoir plus</p>
-                </div>
-
-            <hr />
-                <div>
-                    <h2 className="text-xl font-semibold text-normal-700">Règles de base</h2> <br></br>
-                    <p>
-                        Nous demandons à tous les voyageurs de se souvenir de quelques règles simples <p></p> 
-                        qui contribueront à rendre leur séjour agréable, pour eux comme pour les hôtes.
-                    <li>Respectez le règlement intérieur.</li>
-                    <li>Traitez le logement de votre hôte comme si c&apos;était le vôtre.</li>
-                    </p>
-                </div>
-            <hr />
-            <PayPalButton totalPrice={totalPrice} onSuccess={onFinal} />
-{/*             <hr />
-            <div className="text-lg font-bold text-normal-300">
-            </div>
- */}        </div>
         </Container>
     );
 };
