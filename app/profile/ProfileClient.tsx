60ce166da425e647e5c2782ee0c43a650984505f
Fouad-berry <fidriss96@gmail.com>
2024-08-04 12:02:59 +0200

    update profielclient page

--- app/profile/ProfileClient.tsx
+++ app/profile/ProfileClient.tsx
@@ -9,6 +9,8 @@ import axios from "axios";
 import toast from "react-hot-toast";
 import { formatDistanceToNow } from "date-fns";
 import frLocale from 'date-fns/locale/fr';
+import { FiUser, FiMail, FiLock } from "react-icons/fi";
+import Button from "../components/Button"; // Importer le composant Button
 
 interface TripsClientProps {
     currentUser?: SafeUser;
@@ -17,7 +19,6 @@ interface TripsClientProps {
 const ProfileClient: React.FC<TripsClientProps> = ({
     currentUser
 }) => {
-    const [deletingId, setDeletingId] = useState('');
     const [oldPassword, setOldPassword] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [accountAge, setAccountAge] = useState('');
@@ -37,6 +38,8 @@ const ProfileClient: React.FC<TripsClientProps> = ({
                 newPassword
             });
             toast.success("Mot de passe modifié avec succès !");
+            setOldPassword('');
+            setNewPassword('');
         } catch (error) {
             toast.error("Erreur lors de la modification du mot de passe");
         }
@@ -46,12 +49,12 @@ const ProfileClient: React.FC<TripsClientProps> = ({
         <Container>
             <Heading 
                 title={`Bienvenue sur votre profil, ${currentUser?.name || ''}`}
-                subtitle="Vous trouverez ci dessous les informations relatifs a votre profil."
+                subtitle="Vous trouverez ci-dessous les informations relatives à votre profil."
             />
-            <div className="mt-10 grid place-items-start">
+            <div className="mt-10 flex flex-col items-center">
                 {/* Display user card */}
                 {currentUser && (
-                    <div className=" bg-white rounded-md shadow-lg p-6 flex flex-col items-center mb-6">
+                    <div className="bg-white rounded-lg shadow-lg p-6 flex flex-col items-center mb-6 w-full max-w-lg">
                         {/* User image */}
                         {currentUser.image ? (
                             <Image
@@ -59,7 +62,7 @@ const ProfileClient: React.FC<TripsClientProps> = ({
                                 alt="User"
                                 width={96}
                                 height={96}
-                                className="w-24 h-24 rounded-full mb-4"
+                                className="w-24 h-24 rounded-full mb-4 object-cover"
                             />
                         ) : (
                             <Image
@@ -67,52 +70,60 @@ const ProfileClient: React.FC<TripsClientProps> = ({
                                 alt="Secours"
                                 width={96}
                                 height={96}
-                                className="w-24 h-24 rounded-full mb-4"
+                                className="w-24 h-24 rounded-full mb-4 object-cover"
                             />
                         )}
                         {/* User name */}
-                        <p className="text-xl font-bold mb-2">{currentUser.name}</p>
+                        <p className="text-2xl font-semibold mb-2">{currentUser.name}</p>
                         {/* User email */}
-                        <p className="text-gray-600">{currentUser.email}</p>
-                        <p className="text-gray-600">Compte créé il y a {accountAge}</p>
+                        <p className="text-gray-600 flex items-center"><FiMail className="mr-2"/> {currentUser.email}</p>
+                        <p className="text-gray-600 flex items-center"><FiUser className="mr-2"/> Compte créé il y a {accountAge}</p>
                         {/* Add other user information here */}
                     </div>
                 )}
 
                 {/* Change password form */}
-                <form onSubmit={handleSubmit} className="bg-white rounded-md shadow-lg p-6 flex flex-col items-center">
-                    <p className="text-xl font-bold mb-4">Modifier le mot de passe</p>
-                    <div className="mb-4">
-                        <label htmlFor="oldPassword" className="block text-sm font-medium text-gray-700">
+                <form onSubmit={handleSubmit} className="bg-white rounded-lg shadow-lg p-6 flex flex-col items-center w-full max-w-lg">
+                    <p className="text-2xl font-semibold mb-4">Modifier le mot de passe</p>
+                    <div className="mb-4 w-full">
+                        <label htmlFor="oldPassword" className="block text-sm font-medium text-gray-700 mb-2">
                             Ancien mot de passe :
                         </label>
-                        <input
-                            id="oldPassword"
-                            type="password"
-                            placeholder="Ancien mot de passe"
-                            value={oldPassword}
-                            onChange={(e) => setOldPassword(e.target.value)}
-                            className="w-full p-2 mb-2 border border-gray-300 rounded-md"
-                            required
-                        />
+                        <div className="relative">
+                            <FiLock className="absolute top-3 left-3 text-gray-400"/>
+                            <input
+                                id="oldPassword"
+                                type="password"
+                                placeholder="Ancien mot de passe"
+                                value={oldPassword}
+                                onChange={(e) => setOldPassword(e.target.value)}
+                                className="w-full p-3 pl-10 border border-gray-300 rounded-md"
+                                required
+                            />
+                        </div>
                     </div>
-                    <div className="mb-4">
-                        <label htmlFor="newPassword" className="block text-sm font-medium text-gray-700">
+                    <div className="mb-4 w-full">
+                        <label htmlFor="newPassword" className="block text-sm font-medium text-gray-700 mb-2">
                             Nouveau mot de passe :
                         </label>
-                        <input
-                            id="newPassword"
-                            type="password"
-                            placeholder="Nouveau mot de passe"
-                            value={newPassword}
-                            onChange={(e) => setNewPassword(e.target.value)}
-                            className="w-full p-2 mb-2 border border-gray-300 rounded-md"
-                            required
-                        />
+                        <div className="relative">
+                            <FiLock className="absolute top-3 left-3 text-gray-400"/>
+                            <input
+                                id="newPassword"
+                                type="password"
+                                placeholder="Nouveau mot de passe"
+                                value={newPassword}
+                                onChange={(e) => setNewPassword(e.target.value)}
+                                className="w-full p-3 pl-10 border border-gray-300 rounded-md"
+                                required
+                            />
+                        </div>
                     </div>
-                    <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md">
-                        Modifier
-                    </button>
+                    <Button
+                        label="Modifier"
+                        onClick={() => {}} // Laisser vide car le formulaire gère l'événement onSubmit
+                        className=""
+                    />
                 </form>
             </div>
         </Container>
