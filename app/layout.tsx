41da550be78194b9ea406b105c7fa4249fcdae94
Fouad-berry <fidriss96@gmail.com>
2024-08-11 23:56:17 +0200

    add google tag manager v2

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,17 +38,10 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <Head>
-        {/* Google Tag Manager */}
-        <Script
-          id="gtm-script"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5JFJSVMX');`,
-          }}
+        <title>Atypik house</title>
+        <meta
+          name="description"
+          content="Site de locations de logements alternatifs"
         />
       </Head>
       <body className={roboto.className}>
@@ -61,6 +54,18 @@ export default async function RootLayout({
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
+        {/* Google Tag Manager Script */}
+        <Script
+          id="gtm-script"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5JFJSVMX');`,
+          }}
+        />
         <ClientOnly>
           <ToasterProvider />
           <SearchModal />
