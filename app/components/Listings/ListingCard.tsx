103def966a9e8fbe5b6377e8403ddd8bc075ed4c
Fouad-berry <fidriss96@gmail.com>
2024-08-28 19:32:51 +0200

    update listingcard logic

--- app/components/Listings/ListingCard.tsx
+++ app/components/Listings/ListingCard.tsx
@@ -126,7 +126,7 @@ const ListingCard: React.FC<ListingCardProps> = ({
                 </div>
                 <div className="flex flex-row items-center gap-1">
                     <div className="font-semibold">
-                        € {price}
+                        {price} €
                     </div>
                     {!reservation && (
                         <div className="font-light"> par nuit</div>
