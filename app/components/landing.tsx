a342fbbdd057fa3f92bdb6621ccab739884d29f1
Fouad-berry <fidriss96@gmail.com>
2024-09-12 09:56:30 +0200

    update landing page content

--- app/components/landing.tsx
+++ app/components/landing.tsx
@@ -2,17 +2,20 @@
 
 import React from 'react';
 import Image from 'next/image';
+import Container from './Container';
 
 const Landing = () => {
   return (
-    <div className="relative w-screen h-[800px]">
+    <Container>
+    <div className="relative w-full h-[800px]">
       <Image
-        src="/images/customer.jpg"
+        src="/images/bg-lg.jpg"
         alt="Background Image"
-        style={{ objectFit: 'cover' }}      
+        style={{ objectFit: 'contain' }}      
         fill
       />
     </div>
+    </Container>
   );
 };
 
