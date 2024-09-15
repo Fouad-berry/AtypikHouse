8e8e10fefe8d53d5a72ce9ff2e3fc59882c2efaa
Fouad-berry <fidriss96@gmail.com>
2024-09-15 14:56:40 +0200

    add content up the navbar

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -8,54 +8,63 @@ import Search from "./Search";
 import UserMenu from "./UserMenu";
 import { SafeUser } from "@/app/types";
 import Warning from "../warning";
+import { useRouter } from "next/navigation";
 
 interface NavBarProps {
-    currentUser?: SafeUser | null;
+  currentUser?: SafeUser | null;
 }
 
-const NavBar: React.FC<NavBarProps> = ({
-    currentUser
-}) => {
-    console.log({ currentUser });
+const NavBar: React.FC<NavBarProps> = ({ currentUser }) => {
+  const [showWarning, setShowWarning] = useState<boolean>(false);
+  const router = useRouter();
 
-    const [showWarning, setShowWarning] = useState<boolean>(false);
-  
-    useEffect(() => {
-      if (currentUser) {
-        setShowWarning(true);
-      }
-    }, [currentUser]);
-  
-    const handleDismissWarning = () => {
-      setShowWarning(false);
-    };
-  
-    return (
-        <div className="fixed w-full bg-white z-10 shadow-sm">
-            {showWarning && <Warning onDismiss={handleDismissWarning} />}
-            <div className="py-4 border-b-[1px]">
-                <Container>
-                    <div className="
-                    flex
-                    flex-row
-                    items-center
-                    justify-between
-                    gap-3
-                    ml-6">  {/* Ajout de la marge à gauche */}
-                        <Logo />
-                        <div className="flex flex-col items-center flex-grow">
-                            <div className="text-center text-sm font-bold text-black-700 mb-2">
-                            Découvrez le glamping, les cabanes, les mini-maisons, les cabanes dans les arbres, les dômes...
-                            </div>
-                            <Search />
-                        </div>
-                        <UserMenu currentUser={currentUser} />
-                    </div>
-                </Container>
+  useEffect(() => {
+    if (currentUser) {
+      setShowWarning(true);
+    }
+  }, [currentUser]);
+
+  const handleDismissWarning = () => {
+    setShowWarning(false);
+  };
+
+  return (
+    <div className="fixed w-full bg-white z-10 shadow-sm">
+      {showWarning && <Warning onDismiss={handleDismissWarning} />}
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div className="flex flex-row items-center justify-between gap-3 ml-6">
+            <Logo />
+
+            <div className="flex-grow flex flex-col items-center justify-center space-y-2">
+              <div className="hidden md:flex items-center space-x-4">
+                <div
+                  className="cursor-pointer text-black-700 hover:text-yellow-500 text-[15px] font-bold"
+                  onClick={() => router.push("/")}
+                >
+                  Logements
+                </div>
+                <hr className="h-6 border border-gray-300" />
+                <div
+                  className="cursor-pointer text-black-700 hover:text-yellow-500 text-[15px] font-bold"
+                  onClick={() => router.push("/experiences")}
+                >
+                  Expériences
+                </div>
+              </div>
+
+              <div className="w-full flex justify-center">
+                <Search />
+              </div>
             </div>
-            <Categories />
-        </div>
-    );
+
+            <UserMenu currentUser={currentUser} />
+          </div>
+        </Container>
+      </div>
+      <Categories />
+    </div>
+  );
 };
 
 export default NavBar;
