efaae5e0260d04a23ef7fefb554623d93f7f4c4d
Fouad-berry <fidriss96@gmail.com>
2024-09-15 09:40:48 +0200

    add the logic to warning.tsx

--- app/components/warning.tsx
+++ app/components/warning.tsx
@@ -1,11 +1,24 @@
+"use client";
 import React from 'react';
 
-const warning = () => {
+interface WarningProps {
+  onDismiss: () => void;
+}
+
+const Warning: React.FC<WarningProps> = ({ onDismiss }) => {
   return (
-    <div>
-      
+    <div className="bg-yellow-200 text-yellow-800 p-4 flex justify-between items-center">
+      <span>
+        Ce site est une démonstration pour test, aucune commande ne sera honorée.
+      </span>
+      <button
+        onClick={onDismiss}
+        className="text-yellow-800 font-bold hover:underline focus:outline-none"
+      >
+        Ignorer
+      </button>
     </div>
   );
 };
 
-export default warning;
+export default Warning;
