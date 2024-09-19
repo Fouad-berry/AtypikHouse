a040c1eeb8f7d2780f0095a4a1e0c0546127fa9f
Fouad-berry <fidriss96@gmail.com>
2024-09-20 00:59:01 +0200

    add embla carousel package

--- app/experiences/page.tsx
+++ app/experiences/page.tsx
@@ -5,19 +5,32 @@ import Footer from "../components/Footer";
 import Container from "../components/Container";
 import Heading from "../components/Heading";
 import Image from "next/image";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import EmblaCarousel from "../components/EmblaCarousel";
+import { EmblaOptionsType } from 'embla-carousel'
+import '../embla.css';
+
+
 
 const Experiences = async () => {
 
+    const OPTIONS: EmblaOptionsType = {}
+    const SLIDE_COUNT = 5
+    const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
     return (
         <>
-            <Heading
+{/*             <Heading
                 title="Politique de confidentialité"
             />
-
+ */}
+    <h1 className="bonjour">Bonjour</h1>
+                <EmblaCarousel slides={SLIDES} options={OPTIONS} />
 
             <Footer />
             <MobileNavbar />
-            </>
+        </>
     )
 };
 
