6a302efeba85d3f55840e068fa7da6340e1b3b82
Fouad-berry <fidriss96@gmail.com>
2024-09-22 12:57:43 +0200

    remove unused folder v2

--- app/api/payment/route.ts
+++ app/api/payment/route.ts
@@ -3,7 +3,6 @@ import prisma from '@/app/libs/prismadb';
 import getCurrentUser from '@/app/actions/getCurrentUser';
 
 function generateOrderNumber() {
-  // Vous pouvez utiliser une fonction plus sophistiquée pour générer des numéros de commande aléatoires
   return 'ORD-' + Math.floor(Math.random() * 1000000).toString().padStart(6, '0');
 }
 
